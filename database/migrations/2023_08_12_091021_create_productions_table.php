e3a7d3fd3b151765737ad5e4f82524bb9ea089b5
Srikanth <code.srikanthjanjirala@gmail.com>
2023-08-12 15:15:04 +0530

    tester

--- database/migrations/2023_08_12_091021_create_productions_table.php
+++ database/migrations/2023_08_12_091021_create_productions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Production extends Migration
+class CreateProductionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,13 @@ class Production extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('productions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('section');
+            $table->string('address');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +29,6 @@ class Production extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('productions');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ProductController::class, 'index'])->name('production.index'); 
